4d175edf364bd7f8f2633a33d911c19637a24906
unknown <spdseba@gmail.com>
2022-04-03 19:38:38 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Porfolio Project...</h2>
+    </div>
+  );
+}
+
+export default App;
