2dfb7a835df0548af7ad51dd5061dddae207d4a2
unknown <spdseba@gmail.com>
2022-04-03 20:04:57 -0300

    Segundo Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h2>Porfolio Project...</h2>
+      <Navbar/>
     </div>
   );
 }
