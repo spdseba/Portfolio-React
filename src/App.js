2e72a0cd7d4dcad53d461a01c5438af195099b52
unknown <spdseba@gmail.com>
2022-04-03 21:03:37 -0300

    Tercer Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
 import Navbar from './components/Navbar';
 
 function App() {
